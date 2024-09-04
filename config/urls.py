47fdfc47d5c67b9d19894d4119a9ae4d9bfd3c0f
Jason Heppler <jason.heppler@gmail.com>
2024-09-04 09:25:13 -0500

    refactor: Update database URL path for single textile record view

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from material.views import database, index, textile_records_single
+from material.views import TextileTableView, index, textile_records_single
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,8 +15,6 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("pages/", include(wagtail_urls)),
     path("", index, name="index"),
-    path("database/", database, name="database"),
-    path(
-        "database/<int:item_id>/", textile_records_single, name="textile_records_single"
-    ),
+    path("database/", TextileTableView.as_view(), name="database"),
+    path("database/<int:item_id>/", textile_records_single, name="textile_single"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
