55a4c9f9a974a2063e2cb69b4ef4d9e62c1f44ad
Jason Heppler <jason.heppler@gmail.com>
2024-09-04 09:26:05 -0500

    refactor: Update TextileTable and TextileFilter in material/views.py and material/tables.py

--- material/views.py
+++ material/views.py
@@ -1,31 +1,30 @@
-from django.core.paginator import EmptyPage, Paginator
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django_filters.views import FilterView
-from django_tables2 import RequestConfig
-from taggit.models import Tag
+from django_tables2 import SingleTableMixin
 
-from material.filters import TextileRecordFilter
+from material.filters import TextileFilter
 from material.models import TextileRecord
-from material.tables import TextileRecordTable
+from material.tables import TextileTable
 
 
 def index(request):
     return render(request, "index.html")
 
 
-def database(request):
-    queryset = TextileRecord.objects.all()
-    filter = TextileRecordFilter(request.GET, queryset=queryset)
-    table = TextileRecordTable(filter.qs)
-    RequestConfig(request, paginate={"per_page": 25}).configure(table)
-    return render(
-        request,
-        "database/textile_records_list.html",
-        {"filter": filter, "table": table},
-    )
-
-
 def textile_records_single(request, item_id):
     item = get_object_or_404(TextileRecord, pk=item_id)
     return render(request, "database/textile_records_single.html", {"item": item})
+
+
+class TextileTableView(SingleTableMixin, FilterView):
+    paginate_by = 5
+    table_class = TextileTable
+    filterset_class = TextileFilter
+    # queryset = TextileRecord.objects.all()
+
+    def get_template_names(self):
+        if self.request.htmx:
+            template_name = "database/single_table.html"
+        else:
+            template_name = "database/textiles.html"
+        return template_name
