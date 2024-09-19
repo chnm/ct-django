0199d17def8edb92a74af4bbabb0bfa38ef93bc6
Jason Heppler <jason.heppler@gmail.com>
2024-09-19 13:48:29 -0500

    Refactor URL patterns to include new views and paths

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from exhibits.views import about, concepts, events, lessons
 from material.views import (
     TextileTableView,
     get_secondary_textile_types,
@@ -16,12 +17,18 @@ from material.views import (
 )
 
 urlpatterns = [
+    path("", index, name="index"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    # Wagtail URLs
     path("cms/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("pages/", include(wagtail_urls)),
-    path("", index, name="index"),
+    path("about/", about, name="about"),
+    path("concepts/", concepts, name="concepts"),
+    path("events/", events, name="event"),
+    path("lessons/", lessons, name="lessons"),
+    # Site pages
     path("database/", TextileTableView.as_view(), name="database"),
     path(
         "database/<int:item_id>/", textile_records_single, name="textile_single"
@@ -35,4 +42,6 @@ urlpatterns = [
         get_secondary_textile_types,
         name="get_secondary_textile_types",
     ),
+    # Log in / log out controls
+    path("logout/", index, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
