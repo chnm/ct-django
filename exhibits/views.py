951460db85da2806cb77d27891d64bb78f637584
Jason Heppler <jason.heppler@gmail.com>
2024-09-19 13:49:16 -0500

    feat: Add views to use GeneralPage model for about, concepts, events, and lessons

--- exhibits/views.py
+++ exhibits/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from exhibits.models import GeneralPage
+
+
+def about(request):
+    about_page = GeneralPage.objects.live().filter(title="About").first()
+    return render(request, "exhibits/general_page.html", {"page": about_page})
+
+
+def concepts(request):
+    concepts_page = GeneralPage.objects.live().filter(title="Concepts").first()
+    return render(request, "exhibits/general_page.html", {"page": concepts_page})
+
+
+def events(request):
+    events_page = GeneralPage.objects.live().filter(title="Launch Event").first()
+    return render(request, "exhibits/general_page.html", {"page": events_page})
+
+
+def lessons(request):
+    lessons_page = GeneralPage.objects.live().filter(title="Lesson Plans").first()
+    return render(request, "exhibits/general_page.html", {"page": lessons_page})
