428ee784b59088b5cf52e09fb890d78994fe0dfe
qtrinh2 <qtrinh2@gmu.edu>
2024-09-26 12:43:05 -0400

    enabling minio object storage

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -180,8 +181,24 @@ INTERNAL_IPS = [
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
+# Media files
+OBJ_STORAGE = env("OBJ_STORAGE", default=False)
+if OBJ_STORAGE:
+    AWS_ACCESS_KEY_ID = env("OBJ_STORAGE_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("OBJ_STORAGE_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("OBJ_STORAGE_BUCKET_NAME")
+    AWS_S3_ENDPOINT_URL = env("OBJ_STORAGE_ENDPOINT_URL")
+
+    MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/"
+
+    # override default storage backend for media
+    STORAGES["default"] = {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    }
+else:
+    MEDIA_URL = "media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 
 # Wagtail
