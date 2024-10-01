8b9b065e1cc8416a231ceae08d8273c033c19b0d
Jason Heppler <jason.heppler@gmail.com>
2024-10-01 10:16:58 -0500

    refactor: Add default image attributes to WagtailImagesAppConfig

--- exhibits/apps.py
+++ exhibits/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from wagtail.images.apps import WagtailImagesAppConfig
 
 
 class ExhibitsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "exhibits"
+
+
+class CustomImagesAppConfig(WagtailImagesAppConfig):
+    default_attrs = {"decoding": "async", "loading": "lazy"}
