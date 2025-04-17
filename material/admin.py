a7a54dc2ee6c49afafd6d2d919b51b8fcd4bd958
Jason Heppler <jason.heppler@gmail.com>
2025-04-17 11:13:54 -0500

    build: Add django-unfold and dashboard to admin view

--- material/admin.py
+++ material/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+
 from import_export.admin import ImportExportModelAdmin
+from unfold.admin import ModelAdmin
+from unfold.admin import TabularInline
 
 from material.models import (
     ArchivalRecord,
@@ -23,48 +26,48 @@ from material.resources import (
 )
 
 
-class NamedActorsInline(admin.TabularInline):
+class NamedActorsInline(TabularInline):
     model = NamedActor
 
 
-class ImagesInline(admin.TabularInline):
+class ImagesInline(TabularInline):
     model = Image
 
 
-class PlacesAliasInline(admin.TabularInline):
+class PlacesAliasInline(TabularInline):
     model = PlacesAlias
 
 
-class TextileAliasInline(admin.TabularInline):
+class TextileAliasInline(TabularInline):
     model = TextileAlias
 
 
-class ArchivalRecordInline(admin.TabularInline):
+class ArchivalRecordInline(TabularInline):
     model = ArchivalRecord
 
 
 @admin.register(Area)
-class AreaAdmin(admin.ModelAdmin):
+class AreaAdmin(ModelAdmin):
     resource_class = AreaResource
     list_display = ["name"]
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(ModelAdmin):
     resource_class = PlaceResource
     list_display = ["id", "city", "country", "area", "latitude", "longitude"]
     inlines = [PlacesAliasInline]
 
 
 @admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
+class SubjectAdmin(ModelAdmin):
     resource_class = SubjectResource
     list_display = ["name"]
     search_fields = ["name"]
 
 
 @admin.register(PrimaryTextileType)
-class PrimaryTextileTypeAdmin(admin.ModelAdmin):
+class PrimaryTextileTypeAdmin(ModelAdmin):
     list_display = [
         "name",
         "description",
@@ -73,7 +76,7 @@ class PrimaryTextileTypeAdmin(admin.ModelAdmin):
 
 
 @admin.register(SecondaryTextileType)
-class SecondaryTextileTypeAdmin(admin.ModelAdmin):
+class SecondaryTextileTypeAdmin(ModelAdmin):
     list_display = [
         "name",
         "description",
@@ -81,20 +84,17 @@ class SecondaryTextileTypeAdmin(admin.ModelAdmin):
     inlines = [TextileAliasInline]
 
 
+@admin.action(description="Mark selected records as public")
 def make_public(modeladmin, request, queryset):
     queryset.update(is_public=True)
 
 
-make_public.short_description = "Mark selected records as public"
-
-
+@admin.action(description="Mark selected records as private")
 def make_private(modeladmin, request, queryset):
     queryset.update(is_public=False)
 
 
-make_private.short_description = "Mark selected records as private"
-
-
+@admin.action(description="Unpublish selected records from crawler")
 def unpublish_from_crawler(modeladmin, request, queryset):
     """
     Unpublish records that came from the crawler
@@ -136,11 +136,8 @@ def unpublish_from_crawler(modeladmin, request, queryset):
         )
 
 
-unpublish_from_crawler.short_description = "Unpublish selected records from crawler"
-
-
 @admin.register(TextileRecord)
-class TextileRecordAdmin(ImportExportModelAdmin):
+class TextileRecordAdmin(ModelAdmin, ImportExportModelAdmin):
     resource_class = TextileRecordResource
     list_display = [
         "id",
@@ -169,6 +166,13 @@ class TextileRecordAdmin(ImportExportModelAdmin):
 
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(ModelAdmin):
     resource_class = ImageResource
     list_display = ["textile_record", "image", "description"]
+
+
+@admin.register(NamedActor)
+class NamedActorAdmin(ModelAdmin):
+    list_display = ["name", "textile_record"]
+    search_fields = ["name"]
+    list_filter = ["textile_record"]
